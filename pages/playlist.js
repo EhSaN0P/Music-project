db11222cf9b0621b8f8df98522af351bd96ece7f
MRH <mohammadhoseinnoori876@gmail.com>
2024-10-16 21:06:11 +0330

    super playlist added

--- pages/playlist.js
+++ pages/playlist.js
@@ -1,8 +1,9 @@
-let main = document.querySelector("body > .con-main")
-let section = document.createElement("div")
+
 function playlist(){
     for(let i = 0 ; i < 10 ; i++){
-
+        let main = document.querySelectorAll("body > .song-playlist").item(0)
+        let section = document.createElement("div")
+        
 let playlist = document.createElement("div")
 let img = document.createElement("img")
 let info = document.createElement("div")
@@ -16,11 +17,13 @@ p.innerText = "Lady Gaga"
 
 
 img.src = "image\\Avatar.jpg"
+section.style.marginLeft = "250px"
+section.style.marginBottom = "-50px"
 
 
 
 playlist.style.display = "flex"
-playlist.style.width = "800px"
+playlist.style.width = "1200px"
 playlist.style.height = "200px"
 playlist.style.justifyItems = "center"
 playlist.style.flexDirection = "row"
@@ -28,14 +31,14 @@ playlist.style.color = "white"
 
 
 img.style.width = "15%"
-img.style.height = "50%"
+img.style.height = "60%"
 img.style.marginLeft = "15%"
 img.style.borderBottomLeftRadius = "15px"
 img.style.borderTopLeftRadius = "15px"
 
 info.style.background = "linear-gradient(to right,var(--leftBarBgColor1),var(--leftBarBgColor2))"
 info.style.width = "80%"
-info.style.height = "50%"
+info.style.height = "60%"
 info.style.display = "flex"
 info.style.flexDirection = "row"
 info.style.alignItems = "center"
@@ -61,15 +64,32 @@ info.appendChild(p)
 console.log(main);
 
 
-let playlistBox = document.createElement("div")
-if(checkbox.checked == true)
-    
-    playlistBox.className = ("playlist-box")
-    main.appendChild(playlistBox)
-    playlistBox.appendChild(section)
-    
+
 }
 }
-console.log(main);
+
+
+function list(){
+    let lists = document.querySelectorAll("body > .con-main > .lists").item(0)
+    let songs = document.createElement("div")
+    songs.style.width = "150px"
+    songs.style.height = "150px"
+    songs.style.marginTop = "100px"
+    songs.style.borderRadius = "20px"
+    songs.style.background = "linear-gradient(to right,var(--leftBarBgColor1),var(--leftBarBgColor2))"
+    songs.style.position = "absolute"
+    songs.onclick = function(){
+        songs.style.width = "1000px"
+        songs.style.height = "600px"
+        songs.style.transition = "1s"
+    }
+    songs.onmouseout = function(){
+        songs.style.width = "150px"
+        songs.style.height = "150px"
+        songs.style.transition = "1s"
+    }
+    lists.appendChild(songs)
+}
 playlist()
+list()
 
